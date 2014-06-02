46a95185553427c12cb0d094bd4a115169713ed0
Garrett Honeycutt <code@garretthoneycutt.com>
2014-06-02 10:32:58 -0400

    Some changes after reviewing

--- README.markdown
+++ README.markdown
@@ -1,3 +1,18 @@
-# splunkforwarder #
+This should look like [https://github.com/ghoneycutt/puppet-module-ssh/blob/master/README.md](https://github.com/ghoneycutt/puppet-module-ssh/blob/master/README.md)
 
-This is the splunkforwarder module. It provides...
+The repo should be puppet-module-splunkforwarder
+
+
+Use this skeleton when generating modules
+
+https://github.com/ghoneycutt/puppet-module-skeleton
+
+
+Noticed a lot of messed up spacing. This can help.
+
+Use this vimrc - download - do not cut n paste
+https://raw.githubusercontent.com/ghoneycutt/puppet-module-vim/master/files/vimrc
+
+
+Here's the syntax highlighting for vim
+https://github.com/puppetlabs/puppet-syntax-vim
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,53 +1,47 @@
-# Class: splunkforwarder
+# == Class: splunkforwarder
 #
 # This module manages splunkforwarder
 #
-# Parameters: none
-#
-# Actions:
-#
-# Requires: see Modulefile
-#
-# Sample Usage:
-#
-class splunkforwarder
-(
-  $install_source          = 'undef',
-  $deployment_server       = 'undef',
-  $deployment_server_port  = 'undef',
-)
- {
- package {'universalforwarder':
-    source          => "${::install_source}",
+class splunkforwarder (
+  $install_source          = 'C:\Temp\splunk.msi', # somethign similar to that
+  $deployment_server       = "splunk.${::domain}",
+  $deployment_server_port  = '9997',
+) {
+
+  package { 'universalforwarder':
+    source          => $install_source,
     install_options => {
-      "AGREETOLICENSE"         => 'Yes',
-      "DEPLOYMENT_SERVER"      => "${::deployment_server}:${::deployment_server_port}",
-      "LAUNCHSPLUNK"           => "1",
-      "SERVICESTARTTYPE"       => "auto",
-  }
-}
-    file { 'deploymentserver':
-      ensure   => present,
-      path     => "C:\\Program Files\\SplunkUniversalForwarder\\etc\\system\\local\\deploymentclient.conf",
-      mode     => '0700',
-      content  => template('splunkforwarder/deploymentserver.erb'),
-      require  => Package['universalforwarder'],
-      notify   => Service['splunkforwarder'],
+      'AGREETOLICENSE'    => 'Yes',
+      'DEPLOYMENT_SERVER' => "${::deployment_server}:${::deployment_server_port}",
+      'LAUNCHSPLUNK'      => '1',
+      'SERVICESTARTTYPE'  => 'auto',
     }
-  service {'splunkforwarder':
+  }
+
+  file { 'deploymentserver':
+    ensure   => present,
+    path     => 'C:\\Program Files\\SplunkUniversalForwarder\\etc\\system\\local\\deploymentclient.conf',
+    mode     => '0700',
+    content  => template('splunkforwarder/deploymentserver.erb'),
+    require  => Package['universalforwarder'],
+    notify   => Service['splunkforwarder'],
+  }
+
+  service { 'splunkforwarder':
     ensure  => running,
     enable  => true,
     require => Package['universalforwarder'],
   }
+
 #file_line { 'sudo_rule':
 #  path => 'C:\Program Files\SplunkUniversalForwarder\etc\system\local\inputs',
 #  line => 'host = $fqdn',
 #}
-#ini_setting { "sample setting":
+#ini_setting { 'sample setting':
 #  ensure  => present,
 #  path    => 'C:\\Program Files\\SplunkUniversalForwarder\\etc\\system\\local\\inputs.conf',
 #  section => 'default',
 #  setting => 'host',
-#  value   => "$fqdn",
+#  value   => $fqdn,
 #}
 }
